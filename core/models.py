ea2191904064d405ea2c9ea91efd76b0bc0d134a
Claudio Belo Rodrigues Junior <claudiobelorjr@gmail.com>
2023-07-29 11:09:36 -0300

    Criação dos modelos básicos do projeto

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+from uuid import uuid1
+
+
+def gen_id() -> str:    
+    return uuid1().hex
+
+
+class User(AbstractUser):
+    id = models.CharField('id', max_length=40, primary_key=True, default=gen_id)
+
+
+class BaseModel(models.Model):
+    id = models.CharField('id', max_length=40, primary_key=True, default=gen_id)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    deleted_at = models.DateTimeField('deletado em', null=True, blank=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now=True)
+    
