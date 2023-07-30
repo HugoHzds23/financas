d75db0c0c175f08de81b0e419ff11071e0b057f6
Claudio Belo Rodrigues Junior <claudiobelorjr@gmail.com>
2023-07-29 22:11:57 -0300

    Criação do modelo de categorias

--- core/models.py
+++ core/models.py
@@ -1,16 +1,18 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.utils import timezone
 from uuid import uuid1
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-def gen_id() -> str:    
+
+def gen_id() -> str:
     return uuid1().hex
 
 
 class User(AbstractUser):
-    id = models.CharField('id', max_length=40, primary_key=True, default=gen_id)
-    
+    id = models.CharField(
+        'id', max_length=40, primary_key=True, default=gen_id
+    )
+
     class Meta:
         verbose_name = 'usuário'
         verbose_name_plural = 'usuários'
@@ -18,11 +20,34 @@ class User(AbstractUser):
 
 
 class BaseModel(models.Model):
-    id = models.CharField('id', max_length=40, primary_key=True, default=gen_id)
-    created_at = models.DateTimeField('criado em', auto_now_add=True)
-    deleted_at = models.DateTimeField('deletado em', null=True, blank=True)
-    updated_at = models.DateTimeField('atualizado em', auto_now=True)
-    
+    id = models.CharField(
+        'id', max_length=40, primary_key=True, default=gen_id, editable=False
+    )
+    created_at = models.DateTimeField(
+        'criado em', auto_now_add=True, db_index=True
+    )
+    deleted_at = models.DateTimeField(
+        'deletado em', null=True, blank=True, db_index=True, editable=False
+    )   # TODO: Implement logical exclusion
+    updated_at = models.DateTimeField(
+        'atualizado em', auto_now=True, db_index=True
+    )
+
     class Meta:
         abstract = True
         db_table = 'core_%(class)ss'
+
+
+class Category(BaseModel):
+    owner = models.ForeignKey(
+        'core.User',
+        related_name='owner',
+        verbose_name='dono',
+        on_delete=models.PROTECT,
+        editable=False,
+    )
+    name = models.CharField('nome', max_length=100, db_index=True)
+
+    class Meta:
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
--- spendsmart/settings.py
+++ spendsmart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path, os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -89,7 +90,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-AUTH_USER_MODEL = "core.User"
+AUTH_USER_MODEL = 'core.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
